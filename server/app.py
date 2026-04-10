5238a38d950672f104ff4661fa1fbb10977ae33f
kalyan6305 <pasupuletikalyannagu@gmail.com>
2026-04-10 15:45:28 +0530

    Update all project files

---
+++ server/app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from env import StudyPlannerEnv, Action
+
+app = FastAPI()
+
+env = StudyPlannerEnv()
+
+@app.get("/")
+def home():
+    return {"status":"running"}
+
+@app.post("/reset")
+def reset():
+
+    obs = env.reset()
+
+    return obs.dict()
+
+@app.post("/step")
+def step(action: Action):
+
+    obs, reward, done, _ = env.step(action)
+
+    return {
+        "observation": obs.dict(),
+        "reward": reward,
+        "done": done,
+        "info": {}
+    }
+
+@app.get("/state")
+def state():
+
+    return env.state().dict()
